708acfae5e4e9106c3844ca3b3fdfb14c449fd5b
Mandar007007 <teamm2d07@gmail.com>
2024-02-18 00:00:22 +0530

    app.js update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,8 @@ import ChatBot from './Components/ChatBot/ChatBot'
 import ModelCard from './Components/Models/ModelCard'
 import ProductPage from './Components/Models/ProductPage'
 import Test from './Components/Models/Test'
+import UploadFiles from './Components/DashBoard/UploadFiles'
+import ProfilePage from './Components/Models/ProfilePage'
 
 function App() {
   const dispatch = useDispatch()
